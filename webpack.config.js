5b724961365a80ae9e89484faa31f50c969c45ad
yan-tar <17968850+yan-tar@users.noreply.github.com>
2021-06-30 20:26:38 +0200

    webpack installed

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    watch: true,
+    watchOptions: {
+        ignored: /node_modules/,
+        poll: 1000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: [/node_modules/],
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html'),
+        })
+    ],
+    devServer: {
+        port: 3000,
+        overlay: true,
+        open: true,
+        hot: true,
+        historyApiFallback: true
+    }
+}
