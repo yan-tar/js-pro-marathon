4872c0b8448cf10d5705e4075671f708ff5edf7e
yan-tar <17968850+yan-tar@users.noreply.github.com>
2021-06-30 21:32:53 +0200

    add build modes and update readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # js-pro-marathon
+
+## Разработка
+
+`npm run dev`
+
+## Build
+
+`npm run build:dev` - development mode
+`npm run build` - production mode
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
+const NODE_ENV = process.env.NODE_ENV;
+
 module.exports = {
-  mode: 'development',
+  mode: NODE_ENV || 'development',
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  watch: true,
+  watch: NODE_ENV === 'development',
   watchOptions: {
     ignored: /node_modules/,
     poll: 1000,
@@ -55,4 +57,5 @@ module.exports = {
     hot: true,
     historyApiFallback: true,
   },
+  devtool: NODE_ENV === "development" ? 'source-map' : false
 };
