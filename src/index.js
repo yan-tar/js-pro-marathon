4acd53d1aab4c8818170b0f5497277c8f9c4b94c
yan-tar <17968850+yan-tar@users.noreply.github.com>
2021-07-11 17:42:32 +0200

    use requestAnimationFrame

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const ctx = canvas.getContext('2d');
 const sprW = 48;
 const sprH = 48;
 const shots = 3;
-const animSpeed = 120;
+// const animSpeed = 120;
 let cycle = 0;
 let sprPos = 0; // spr row
 let posX = 0;
@@ -121,53 +121,62 @@ document.addEventListener('keyup', keyUpHandler);
 const terrain = document.createElement('img');
 terrain.src = terrainAtlas;
 
-terrain.addEventListener('load', () => {
-  const { map } = worldCfg; // деструктурируем и вытаскиваем (только) поле map
+const girlImg = document.createElement('img');
+girlImg.src = GirlWalk;
+
+function showBackground(worldCfgBg) {
+  const { map } = worldCfgBg; // деструктурируем и вытаскиваем (только) поле map
   map.forEach((cfgRow, y) => {
     cfgRow.forEach((cfgCell, x) => {
       const [sX, sY, sW, sH] = sprites.terrain[cfgCell[0]].frames[0];
       ctx.drawImage(terrain, sX, sY, sW, sH, x * sprW, y * sprH, sprW, sprH);
     });
   });
+}
+
+// timestamp - время, прошедшее с начала загрузки страницы в мс
+function walk(timestamp) {
+  console.log('### timestamp ###', timestamp);
+  // двигаем персонажа
+  if (keyPressed) {
+    switch (keyPressed) {
+      case 'down':
+        posY += step;
+        sprPos = 0;
+        break;
+      case 'up':
+        posY -= step;
+        sprPos = 3;
+        break;
+      case 'right':
+        posX += step;
+        sprPos = 2;
+        break;
+      case 'left':
+        posX -= step;
+        sprPos = 1;
+        break;
+      default:
+        break;
+    }
+    cycle = (cycle + 1) % shots; // вычисляем координаты показываемого спpайта
+  }
+  // заставляем персонажа остановиться
+  if (posX > canvasHEnd) posX = canvasHEnd;
+  if (posX < 0) posX = 0;
+  if (posY > canvasVEnd) posY = canvasVEnd;
+  if (posY < 0) posY = 0;
+  // все чистим и показываем новую картинку
+  ctx.clearRect(0, 0, cnvW, cnvH);
+  showBackground(worldCfg);
+  ctx.drawImage(girlImg, sprW * cycle, sprH * sprPos, sprW, sprH, posX, posY, sprW, sprH);
+  window.requestAnimationFrame(walk);
+}
+
+terrain.addEventListener('load', () => {
+  showBackground(worldCfg);
 });
 
-const girlImg = document.createElement('img');
-girlImg.src = GirlWalk;
-
 girlImg.addEventListener('load', () => {
-  setInterval(() => {
-    // двигаем персонажа
-    if (keyPressed) {
-      switch (keyPressed) {
-        case 'down':
-          posY += step;
-          sprPos = 0;
-          break;
-        case 'up':
-          posY -= step;
-          sprPos = 3;
-          break;
-        case 'right':
-          posX += step;
-          sprPos = 2;
-          break;
-        case 'left':
-          posX -= step;
-          sprPos = 1;
-          break;
-        default:
-          break;
-      }
-      cycle = (cycle + 1) % shots; // вычисляем координаты показываемого спpайта
-    }
-    // заставляем персонажа остановиться
-    if (posX > canvasHEnd) posX = canvasHEnd;
-    if (posX < 0) posX = 0;
-    if (posY > canvasVEnd) posY = canvasVEnd;
-    if (posY < 0) posY = 0;
-    // все чистим и показываем новую картинку
-    ctx.clearRect(0, 0, cnvW, cnvH);
-    // showBackground();
-    ctx.drawImage(girlImg, sprW * cycle, sprH * sprPos, sprW, sprH, posX, posY, sprW, sprH);
-  }, animSpeed);
+  window.requestAnimationFrame(walk);
 });
